b59a96e11afafdd00163129b4f0d5cfb7f0cbddf
andmerk93 <andmerk93@yandex.ru>
2023-08-22 03:20:34 +0700

    rev 03

--- app/services/invest.py
+++ app/services/invest.py
@@ -14,14 +14,11 @@ def investment(
     if not target.invested_amount and target.invested_amount != 0:
         target.invested_amount = 0
     for source in sources:
-        to_invest = min(target.full_amount - target.invested_amount,
-                        source.full_amount - source.invested_amount)
-        if to_invest == 0:
-            break
+        to_invest = target.full_amount - target.invested_amount
         for obj in (target, source):
             obj.invested_amount += to_invest
             if obj.full_amount == obj.invested_amount:
                 obj.close_date = datetime.now()
                 obj.fully_invested = True
-            list_modified.append(obj)
+        list_modified.append(source)
     return list_modified
